56b0d68ecf1cf54c4c8b062ea7535ff01fec499c
Eduardo <eduvk11@hotmail.com>
2025-03-16 13:31:11 +0300

    generar crpetas ios y android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'flamenco',
-  webDir: 'dist'
+  webDir: 'dist/flamenco'
 };
 
 export default config;
